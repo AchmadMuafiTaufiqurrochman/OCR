6da4d6efdebf50888d355d264434ec0d141f744b
kreatifa <kreatif.purnama@gmail.com>
2025-03-21 12:38:03 +0700

    changing method rest api to upload file form-data

--- src/main.py
+++ src/main.py
@@ -1,18 +1,31 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile, File
+import shutil
+import os
 from services.OCR import process_ocr
 
 app = FastAPI()
+TEMP_FOLDER = "public/images"
 
 @app.get("/")
 def root():
-    return{"uwu"}
+    return {"message": "uwu"}
 
-
-@app.get("/ktp")
-def run_ocr(image_path: str):
+@app.post("/ktp")
+def run_ocr(file: UploadFile = File(...)):
     try:
-        result = process_ocr(image_path)
+        temp_file_path = os.path.join(TEMP_FOLDER, file.filename)
+        
+        with open(temp_file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        
+        # Proses OCR
+        result = process_ocr(temp_file_path)
+        
+        # Hapus file setelah diproses
+        os.remove(temp_file_path)
+        
         return {"status": "success", "data": result}
+        
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="File tidak ditemukan")
     except Exception as e:
